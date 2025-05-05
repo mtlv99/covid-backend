e400c64a5f0ef9026fd18a79c2285fb7195ecce4
Marco Leon <marco.tulio.leon@gmail.com>
2025-05-04 23:28:50 -0600

    Allow cors

--- covid_images_backend/settings.py
+++ covid_images_backend/settings.py
@@ -4,16 +4,20 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.staticfiles',
     'images',
 ]
 
-MIDDLEWARE = []
+MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware']
 
 ROOT_URLCONF = 'covid_images_backend.urls'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = []
+
+# This is a dummy setting for CORS. In production, you should specify allowed origins.
+CORS_ALLOW_ALL_ORIGINS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'training/Covid19-dataset')
