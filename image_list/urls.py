6bbb7c1aee6c13b6c4957036492586fe9093656d
Marco Leon <marco.tulio.leon@gmail.com>
2025-05-05 02:55:12 -0600

    Add param to image endpoint

--- image_list/urls.py
+++ image_list/urls.py
@@ -3,6 +3,6 @@ from .views import image_list, upload_and_filter_image, predict_image
 
 urlpatterns = [
     path('images/', image_list),
-    path('upload/', upload_and_filter_image),
+    path('process_image/', upload_and_filter_image),
     path('predict/', predict_image),
 ]
